eaed1690740127ed3042e52f90d87e6878646812
Mihó Dániel <miho.daniel@mdy.hu>
2025-11-23 00:58:44 +0100

    feat(docs): add version to config

---
+++ config/version.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used
+    | by Scramble to display the API version, and may be used elsewhere.
+    | It is automatically updated during the deployment process.
+    |
+    */
+
+    'number' => '0.0.0-dev',
+];
