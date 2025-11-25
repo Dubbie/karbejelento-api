ff993ea8a3af4bd4f3dc133801650e01e3b90f53
Mihó Dániel <miho.daniel@damarisk.hu>
2025-11-25 15:29:35 +0100

    style(document-requests): trim document request mail to only include link to frontend

--- resources/views/emails/report-document-request.blade.php
+++ resources/views/emails/report-document-request.blade.php
@@ -5,37 +5,12 @@
     <title>{{ $emailTitle ?? 'Document Request' }}</title>
 </head>
 <body>
-    <p>Dear Partner,</p>
-
-    <p>{!! nl2br(e($emailBody)) !!}</p>
-
-    @if(!empty($requestedDocuments))
-        <p>The following documents are requested:</p>
-        <ul>
-            @foreach($requestedDocuments as $document)
-                <li>{{ $document }}</li>
-            @endforeach
-        </ul>
-    @endif
+    {!! nl2br(e($emailBody)) !!}
 
     @if(!empty($documentRequestUrl))
         <p>
-            You can upload the requested files using the following secure link:<br>
             <a href="{{ $documentRequestUrl }}">{{ $documentRequestUrl }}</a>
         </p>
     @endif
-
-    @if(!empty($otherDocumentNote))
-        <p><strong>Additional notes:</strong> {{ $otherDocumentNote }}</p>
-    @endif
-
-    <p>
-        <strong>Report details</strong><br>
-        Damage ID: {{ $report->damage_id ?? 'Not provided' }}<br>
-        Building: {{ $report->building->name ?? 'N/A' }}<br>
-        Report Reference: {{ $report->uuid }}
-    </p>
-
-    <p>Thank you,<br>The DAMArisk Team</p>
 </body>
 </html>
